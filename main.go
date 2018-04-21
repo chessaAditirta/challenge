580661784dffbd321e4be44267a7172b3b17ba86
SatriaNg <ngestusatria7@gmail.com>
2018-04-21 16:07:05 +0700

    Commit Contact

--- main.go
+++ main.go
@@ -9,7 +9,13 @@ func home(w http.ResponseWriter, r *http.Request){
   fmt.Fprintf(w, "Halaman Awal")
 }
 
+func contact(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "Halaman Kontak")
+}
+
+
 func main(){
   http.HandleFunc("/", home)
+  http.HandleFunc("/contact", contact)
   http.ListenAndServe(":8080", nil)
 }
