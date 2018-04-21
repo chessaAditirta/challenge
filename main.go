90b726e839eaa703e01059b78485c448111ef994
chessaAditirta <chessatirta@gmail.com>
2018-04-21 16:17:07 +0700

    0.1.1:Halaman info

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func home(w http.ResponseWriter, r *http.Request){
   fmt.Fprintf(w, "Halaman Awal")
 }
 
+func info(w http.ResponseWriter, r *http.Request){
+  fmt.Fprintf(w, "Ini kayaknya Halaman Info")
+}
+
 func main(){
   http.HandleFunc("/", home)
+  http.HandleFunc("/kampret", info)
   http.ListenAndServe(":8080", nil)
 }
